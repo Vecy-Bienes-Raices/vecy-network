3de855a62e275580b5e2d72660a437c8bd2feb55
Eduardo - Vecy Network <eduardo@vecy.network>
2026-05-19 01:07:14 -0500

    FINAL TRIGGER: Deploying latest JanIA Console with corrected lockfile

--- server/_core/janIA.ts
+++ server/_core/janIA.ts
@@ -1,6 +1,6 @@
 /**
  * JanIA Core Logic - VECY Network
- * Version: 2.0.1 (JanIA Console Update)
+ * Version: 2.0.2 (JanIA Console Update)
  */
 import { invokeLLM } from "./llm";
 import { getDb } from "../db";
